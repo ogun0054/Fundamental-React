e6658f229ab3334ae68df0a27da374d7a2e8dd29
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2026-01-05 21:56:30 -0500

    advice app

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,33 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    // alert("Clicking");
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
   return (
     <div className="App">
-      <Header />
-      <InputForm />
+      <h1>{advice}</h1>
+      <Button getAdvice={getAdvice}>Get Advice</Button>
+      <Message count={count} />
     </div>
   );
 }
 
 export default App;
 
-function Header() {
-  return (
-    <div>
-      <h1>Get Things Done!</h1>
-    </div>
-  );
+function Button({ children, getAdvice }) {
+  return <button onClick={getAdvice}> {children} </button>;
 }
 
-function InputForm() {
-  return (
-    <form>
-      <input type="text" placeholder="What's the task today?" />
-      <Button>Add Task</Button>
-    </form>
-  );
-}
-
-function Button({ children }) {
-  return <button> {children} </button>;
+function Message({ count }) {
+  return <p>You have read {count} piece(s) of advice</p>;
 }
