54aa70e1e4a867b742ee367c7e43a1e657f2d2fc
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2026-02-01 21:05:50 -0500

    update

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -118,17 +118,21 @@ function Footer(){
 
   return (
   <footer className="footer"> {isOpen ?( 
-    <div className="order">
-      <p>We're open until {closeHour}:00. Come visit us or order online!</p>
-      <button className="btn">Order Now!</button>
-    </div>
+      <Order closeHour={closeHour}/>
     ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>}
   </footer>
   )
   
 }
 
-
+function Order(props){
+  return (
+    <div className="order">
+      <p>We're open until {props.closeHour}:00. Come visit us or order online!</p>
+      <button className="btn">Order Now!</button>
+    </div>
+    )
+}
 
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
