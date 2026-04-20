c13e2f51857add55a64c943485bf35977fb53f9d
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2026-04-19 23:15:07 -0400

    update

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,9 @@
+ const initialItems = [
+   { id: 1, description: "Passports", quantity: 2, packed: false },
+   { id: 2, description: "Socks", quantity: 12, packed: true },
+    { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App(){
   return (
     <div className="app">
@@ -10,6 +16,17 @@ export default function App(){
 
 }
 
+function Item({item}){
+  return <li> 
+    <span style={item.packed? {textDecoration: 'line-through'}: {}}>
+      {item.quantity} {item.description}
+    </span>
+    <button>❌</button>
+    </li>
+}
+
+
+
 function Logo(){
   return <h1>🌴 Far Away 💼</h1>
 }
@@ -21,7 +38,11 @@ function Form(){
 }
 
 function PackingList(){
-  return <div className="list">List</div>
+  return <div className="list">
+      <ul>
+        {initialItems.map(item => <Item item={item} />)}
+      </ul>
+    </div>
 }
 
 function Stats(){
